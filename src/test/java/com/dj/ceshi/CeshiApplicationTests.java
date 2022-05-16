be6ff338baea124bf5fb3b85a9b07720490bc982
dongzhihui <15910339554@163.com>
2022-05-16 12:48:35 +0800

    第一次提交

---
+++ src/test/java/com/dj/ceshi/CeshiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dj.ceshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeshiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
